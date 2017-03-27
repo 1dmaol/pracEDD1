9459b6fee28e4865bc9666e8a071db68a4e582eb
marc <marc@equipo-salon>
2017-03-27 10:25:43 +0200

    Pujant canvi 1 Netbeans

--- RepoEDD1/src/git/GIT.java
+++ RepoEDD1/src/git/GIT.java
@@ -6,6 +6,7 @@ public class GIT {
     	System.out.print ("Hello World.");
     	System.out.println ("IDE Eclipse");
     	System.out.println("");
+        System.out.println("IDE Netbeans");
     }
     
 }
