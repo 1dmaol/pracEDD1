8894bd9f7fe2e94317b672e923727712bf7e4f9a
marc <marc@equipo-salon>
2017-03-27 09:28:23 +0200

    Segona modificació IDE Eclipse

--- RepoEDD1/src/git/GIT.java
+++ RepoEDD1/src/git/GIT.java
@@ -5,6 +5,7 @@ public class GIT {
     public static void main(String[] args) {
     	System.out.print ("Hello World.");
     	System.out.println ("IDE Eclipse");
+    	System.out.println("");
     }
     
 }
